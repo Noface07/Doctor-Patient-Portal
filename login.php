6be97fd22fd56bfdbf772c20db7e1582061b3d5d
Noface07 <yuvrajsoni7@gmail.com>
2021-10-26 04:36:22 +0530

    php adding need time

---
+++ login.php
@@ -0,0 +1,25 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<section class="signup-form">
+<h2>Login</h2>
+<form action="includes/login.inc.php" method="post">
+
+<input type="text" name="name" placeholder="Username/Email">
+<input type="password" name="pwd" placeholder="Password">
+<button type="submit" name="submit">Login</button>
+
+
+
+</form>
+</section>
+
+</body>
+</html>
+
---
+++ signup.php
@@ -0,0 +1,27 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<section class="signup-form">
+<h2>Sign Up</h2>
+<form action="includes/signup.inc.php" method="post">
+
+<input type="text" name="name" placeholder="Full Name">
+<input type="text" name="email" placeholder="Email">
+<input type="text" name="uid" placeholder="Username">
+<input type="password" name="pwd" placeholder="Password">
+<input type="password" name="pwdrepeat" placeholder="Repeat Password">
+<button type="submit" name="submit"> Sign Up</button>
+
+
+
+</form>
+</section>
+
+</body>
+</html>
