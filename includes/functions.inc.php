8757e252c637e92f35ada54db57dd7729eb8b37c
Noface07 <yuvrajsoni7@gmail.com>
2021-10-26 14:26:23 +0530

    php problem

--- includes/functions.inc.php
+++ includes/functions.inc.php
@@ -68,12 +68,35 @@ function uidExists($conn, $username,$email) {
 
     $resultData = mysqli_stmt_get_result($stmt);
 
-    if(mysqli_fetch_assoc($resultData)){
-
+    if($row = mysqli_fetch_assoc($resultData)){
+        return $row;
     }
     else{
         $result = false;
         return  $result;
     }
 
+    mysqli_stmt_close($stmt);
+}
+
+function createUser($conn,$name,$email,$username,$pwd) {
+
+    $sql= "INSERT INTO users (usersName, usersEmail,usersId,usersPwd) VALUES (?,?,?,?);";
+
+    $stmt = mysqli_stmt_init($conn);
+
+    if (!mysqli_stmt_prepare($stmt,$sql)) {
+        header("location ../signup.php?error=stmtfailed");
+        exit();
+    }
+
+    $hashedPwd = password_hash($pwd,PASSWORD_DEFAULT);
+
+    mysqli_stmt_bind_param($stmt, "ssss",$name,$email,$username,$hashedPwd); 
+    mysqli_stmt_execute($stmt);
+
+    mysqli_stmt_close($stmt);
+
+    header("location ../login_page.html?error=none");
+    exit();
 }
